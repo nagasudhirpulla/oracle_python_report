f25c10909b035341ea8e4005674893c7500e8268
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-06-24 13:40:18 +0530

    user management notes updated

--- db_access_poc.py
+++ db_access_poc.py
@@ -13,10 +13,39 @@ SQL> create user user_read_only identified by password;
 SQL> grant create session to user_read_only;
 SQL> grant select any table to user_read_only;
 
-Now select data from schema. Remember to address the main schema by hr.table_name
+# create user with all privileges - https://stackoverflow.com/questions/22386976/create-a-user-with-all-privileges-in-oracle
+create user REPORTING_WEB_UI_UAT identified by 123;
+grant create session to REPORTING_WEB_UI_UAT;
+grant all privileges to REPORTING_WEB_UI_UAT;
+
+# drop a user with all the associated schemas
+DROP USER sidney CASCADE;
+
+# grant particular privileges to particular user - https://chartio.com/resources/tutorials/how-to-create-a-user-and-grant-permissions-in-oracle/
+GRANT
+  SELECT,
+  INSERT,
+  UPDATE,
+  DELETE
+ON
+  schema.books
+TO
+  books_admin;
+
+# revoke all privileges from user - https://stackoverflow.com/questions/30313462/how-to-revoke-all-privileges-for-a-user-in-sqlplus
+revoke all privileges from user_name
+
+# grant permissions to only tables of a particular owner to a user - https://stackoverflow.com/questions/187886/grant-select-on-all-tables-owned-by-specific-user
+BEGIN
+   FOR R IN (SELECT owner, table_name FROM all_tables WHERE owner='TheOwner') LOOP
+      EXECUTE IMMEDIATE 'grant select on '||R.owner||'.'||R.table_name||' to TheUser';
+   END LOOP;
+END; 
+
+# Now select data from schema. Remember to address the main schema by hr.table_name
 SQL> select * from hr.employees
 
-Use drop user command to remove a user
+Use drop user command to just remove a user
 SQL> drop user user_read_only
 
 using python cx_Oracle post - http://www.oracle.com/technetwork/articles/dsl/python-091105.html
